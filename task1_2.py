46e9dafbd34643a06488fe6a378d8479ba6f973c
Sneha-Gupta951 <snehagupta0203200@gmail.com>
2025-08-17 10:48:11 +0530

    complete task1_2

---
+++ task1_2.py
@@ -0,0 +1,13 @@
+# task1
+
+a = int(input("Enter a number: "))
+
+if(a%2 == 0):
+    print(a, "is an even number.")
+else:
+    print(a, "is an odd number.")
+
+# task2
+
+for i in range (1,51):
+    print(i)
